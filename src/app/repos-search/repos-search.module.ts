a20629be4273cb94d85ef51965a7d5f5b29bb02a
Krzysztof Rytlewski <krzysztof.rytlewski@o2.pl>
2020-07-18 08:09:52 +0200

    Routes added

--- src/app/repos-search/repos-search.module.ts
+++ src/app/repos-search/repos-search.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ReposSearchComponent } from './repos-search.component';
+import { RouterModule } from '@angular/router';
 
+import { ReposSearchComponent } from './repos-search.component';
 
 
 @NgModule({
   declarations: [ReposSearchComponent],
   imports: [
+    RouterModule.forChild([
+      { path: '', component: ReposSearchComponent }
+    ]),
     CommonModule
   ]
 })
