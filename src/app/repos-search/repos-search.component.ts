d5c8cd572776041f4d534fe36147036a73266079
Krzysztof Rytlewski <krzysztof.rytlewski@o2.pl>
2020-07-18 07:54:38 +0200

    ReposSearch component added

---
+++ src/app/repos-search/repos-search.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-repos-search',
+  templateUrl: './repos-search.component.html',
+  styleUrls: ['./repos-search.component.scss']
+})
+export class ReposSearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/repos-search/repos-search.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReposSearchComponent } from './repos-search.component';
+
+
+
+@NgModule({
+  declarations: [ReposSearchComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class ReposSearchModule { }
